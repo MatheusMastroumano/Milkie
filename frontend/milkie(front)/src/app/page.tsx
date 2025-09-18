6e5b654f3faa3bb840ab35e13251714fc44182f2
rossato08 <gabriel.rossato050208@gmail.com>
2025-09-18 14:32:29 -0300

    começo do projeto
    começando o projeto

---
+++ frontend/milkie(front)/src/app/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      
+      <main className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold text-center text-blue-600">
+          Bem-vindo à Minha Aplicação Next.js
+        </h1>
+        <p className="mt-4 text-lg text-gray-700 text-center">
+          Esta é uma aplicação simples construída com Next.js e Tailwind CSS.
+        </p>
+        <div className="mt-6 flex justify-center space-x-4">
+  
+  
+        </div>
+      </main>
+    </div>
+  );
+}
