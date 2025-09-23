ca0abb0f896ed5295f96022649d2fc7284cdbc52
Yanpemn <victorcestafran08@gmail.com>
2025-09-23 13:50:39 -0300

    API: start

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>API rodando</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`API rodando em: http://localhost:${port}`);
+});
