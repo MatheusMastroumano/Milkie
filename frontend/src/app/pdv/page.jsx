8854e6b10f7f96ca01ff610e8e61c1237225434f
Luked616 <theluked616@gmail.com>
2025-09-25 13:40:57 -0300

    Criação da página de pdv

---
+++ frontend/src/app/pdv/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "@/components/Header/page";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+        <br></br>
+           <br></br>
+              <br></br>
+    <Header />
+
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Pdv Sigma</h1>
+        <p className="mt-4 text-black-600">Uhhh coisas</p>
+      </main>
+    </div>
+  );
+}
+
