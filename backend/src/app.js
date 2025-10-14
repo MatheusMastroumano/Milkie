27d61fe9c9719405d8be1646e3d4b905d39a91c4
VICTOR CESTARI DE FRANÇA <24250352@educ123.sp.senai.br>
2025-10-14 14:06:12 -0300

    identando

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,15 @@ import dotenv from 'dotenv';
 /* -------------------------------------------------------------------------- */
 /*                            Importação de rotas                             */
 /* -------------------------------------------------------------------------- */
-import { funcionariosRoutes, usuariosRoutes, lojasRoutes, produtosRoutes, precosRoutes, estoqueRoutes } from './modules/index.js';
+import {
+    lojasRoutes,
+    funcionariosRoutes,
+    usuariosRoutes,
+    produtosRoutes,
+    precosRoutes,
+    estoqueRoutes,
+    fornecedoresRoutes
+} from './modules/index.js';
 
 /* -------------------------------------------------------------------------- */
 /*                         Configurações iniciais                             */
@@ -23,37 +31,36 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
 /* ------------------- Middleware global de tratamento de erros -------------- */
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ mensagem: err.message || 'Erro interno do servidor' });
+    console.error(err.stack);
+    res.status(500).json({ mensagem: err.message || 'Erro interno do servidor' });
 });
 
 /* -------------------------------------------------------------------------- */
 /*                                   Rotas                                    */
 /* -------------------------------------------------------------------------- */
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>API rodando</h1>');
+    res.status(200).send('<h1>API rodando</h1>');
 });
 
+app.use('/lojas', lojasRoutes);
 app.use('/funcionarios', funcionariosRoutes);
 app.use('/usuarios', usuariosRoutes);
-app.use('/lojas', lojasRoutes);
-app.use('/fornecedores', fornecedoresRoutes)
 app.use('/produtos', produtosRoutes);
 app.use('/precos', precosRoutes);
-app.use('/precos', precosRoutes);
 app.use('/estoque', estoqueRoutes);
+app.use('/fornecedores', fornecedoresRoutes)
 
 /* -------------------------------------------------------------------------- */
 /*                                Inicialização                               */
 /* -------------------------------------------------------------------------- */
 app.listen(port, () => {
-  console.log(`✅ API rodando em: http://localhost:${port}`);
+    console.log(`✅ API rodando em: http://localhost:${port}`);
 });
