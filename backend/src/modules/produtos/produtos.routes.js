da6d7fdfb01e0033c79d1cc0cd14346e875d66b4
VictorCestari <victorcestafran08@gmail.com>
2025-11-13 16:14:05 -0300

    ajustes

--- backend/src/modules/produtos/produtos.routes.js
+++ backend/src/modules/produtos/produtos.routes.js
@@ -8,7 +8,6 @@ import validate from '../../shared/middlewares/validate.js';
 import authMiddleware from '../../shared/middlewares/authMiddleware.js';
 import moduleAccess from '../../shared/middlewares/moduleAccess.js';
 
-
 const router = express.Router();
 
 // aplica auth e moduleAccess em todas as rotas
