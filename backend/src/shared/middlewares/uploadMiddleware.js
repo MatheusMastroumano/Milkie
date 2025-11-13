97de09c99c9e76d2342cf1fcd54d95d48cd17105
VictorCestari <victorcestafran08@gmail.com>
2025-11-13 16:48:24 -0300

    remove: supabase, add (.env.example): cloudinary

--- backend/src/shared/middlewares/uploadMiddleware.js
+++
@@ -1,8 +0,0 @@
-import fileUpload from 'express-fileupload';
-
-const uploadMiddleware = fileUpload({
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
-  abortOnLimit: true,
-});
-
-export default uploadMiddleware;
